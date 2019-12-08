847fb9c7887c4353a8c1f36686a18d5a28471b83
Matthew B Gardner <mbgardner21@gmail.com>
2019-12-08 12:56:13 -0500

    Bumped version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Logrex.Mixfile do
   def project do
     [
       app: :logrex,
-      version: "0.4.2",
+      version: "0.5.0",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
